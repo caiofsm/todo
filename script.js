d07a01906ae077a8a446ca7884f89ae3853653e1
Caio Fernando <caio.fsm@gmail.com>
2022-06-20 08:40:15 -0300

    adicionado capacidade de remover item

--- script.js
+++ script.js
@@ -3,22 +3,40 @@ console.log("teste")
 const form = document.getElementById('form')
 const ulElement = document.getElementById('items')
 const inputElement = document.getElementById('inputText')
-console.log(form)
+
 form.addEventListener('submit', addItem);
+let itemCount=0
+
 
 
 
 function addItem(evento)
 {
     evento.preventDefault()
-    //console.log(evento.target[0].value)
     const newItem = evento.target[0].value
-
+    
     let newElement = document.createElement('li')
-
     newElement.classList.add('item')
-    newElement.appendChild(document.createTextNode(newItem))
-    ulElement.insertAdjacentElement("beforeend" ,newElement)
+    newElement.id = 'item' + itemCount
+    //newElement.appendChild(document.createTextNode(newItem))
+    let pElement = document.createElement('p')
+    pElement.appendChild(document.createTextNode(newItem))
+    newElement.appendChild(pElement)
+    // console.dir(newElement)
+    ulElement.insertAdjacentElement('afterbegin', newElement)
+    
+    let removeButtonElement = document.createElement('a')
+    removeButtonElement.classList.add('removeButton')
+    removeButtonElement.appendChild(document.createTextNode('x'))
+    removeButtonElement.setAttribute('href', '#')
+    removeButtonElement.addEventListener('click', removeButton)
+    newElement.insertAdjacentElement("beforeend", removeButtonElement)
 
-    inputElement.textContent=""
+    inputElement.textContent=''
+    itemCount++
+}
+
+function removeButton(evento)
+{
+    evento.target.parentElement.remove();
 }
